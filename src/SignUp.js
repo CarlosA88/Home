66c36cac5f589eef000d314cd98d426648bd842f
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtekclients.com>
2020-07-14 17:13:46 -0700

    Changing the name of the sign up form

--- src/SignUp.js
+++ src/SignUp.js
@@ -69,7 +69,7 @@ const SignUp = ({ history }) => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign Up
         </Typography>
         <form className={classes.form} noValidate onSubmit={handleSignUp}>
           <TextField
